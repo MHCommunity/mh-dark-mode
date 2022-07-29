d99b1239827a246d204a82efa90b0d6ddbc3f7d1
Angelo <ramuzuconcepcion@gmail.com>
2022-07-29 15:19:07 +0800

    Added assets folder

--- ui/popup.js
+++ ui/popup.js
@@ -1,21 +1,11 @@
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-
-//trigger on click
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: applyDarkMode,
-    });
-});
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         function: doSomething,
+//     });
+// });
 
 //trigger on load
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
